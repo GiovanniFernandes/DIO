3c8dfb5f171e745b5f7058f01762dd8830e9096e
GiovanniFernandes <giovannifernandes1105@gmail.com>
2022-02-22 15:50:33 -0300

    aula3 finalizada

---
+++ Aula3/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Ricardo";
+p1.Idade = 30;
+p1.EnderecoPessoa = default;
+// p1.EnderecoPessoa = new Endereco(){
+//     Numero = 241,
+//     Logradouro = "tananan",
+// }
