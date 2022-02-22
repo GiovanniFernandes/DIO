4e5824df3607a55fc36387bdcb2c4ecdf5a73e5d
GiovanniFernandes <giovannifernandes1105@gmail.com>
2022-02-22 15:01:03 -0300

    Aula2 finalizada

--- Aula2/Program.cs
+++ Aula2/Program.cs
@@ -18,7 +18,7 @@ class Aula2{
             }
         }
     }
-    static void Main(){
+    static void Demo2(){
         var nomes = new string[]{"José", "Maria", "Ricardo", "Alice", "Pedro"};
         WriteLine($"Lista de nomes: {string.Join(", \n", nomes)}");
         WriteLine("Qual nome você quer mudar? ");
@@ -27,5 +27,49 @@ class Aula2{
         var nomeNovo = ReadLine();
         AlterarNome(nomes, nome, nomeNovo);
         WriteLine($"Lista de nomes: {string.Join(", \n", nomes)}");
+    }
+
+    static int LocalizaNome(string[] nomes, string nome){
+        for(int i=0; i < nomes.Length; i++){
+            if(nomes[i] == nome) return i;
+        }
+        return -1;
+    }
+    static void Demo3(){
+        var nomes = new string[]{"José", "Maria", "Ricardo", "Alice", "Pedro"};
+        WriteLine($"Lista de nomes: {string.Join(", \n", nomes)}");
+        WriteLine("Qual nome você quer mudar? ");
+        var nome = ReadLine();
+
+        var indice = LocalizaNome(nomes, nome);
+        if(indice == -1) WriteLine("Nome não se encontra na lista");
+        else{
+            WriteLine("Para qual nome você quer mudar? ");
+            var nomeNovo = ReadLine();
+            nomes[indice] = nomeNovo;
+            WriteLine($"Lista de nomes: {string.Join(", \n", nomes)}");
+        }
+    }
+
+    static ref string LocalizaNome_com_ref(string[] nomes, string nome){
+        for(int i=0; i < nomes.Length; i++){
+            if(nomes[i] == nome) return ref nomes[i];
+        }
+        throw new Exception("Nome não encontrado");
+    }
+    static void Main(){
+        var nomes = new string[]{"José", "Maria", "Ricardo", "Alice", "Pedro"};
+        WriteLine($"Lista de nomes: {string.Join(", \n", nomes)}");
+        WriteLine("Qual nome você quer mudar? ");
+        var nome = ReadLine();
+
+        ref var nomeAchado = ref LocalizaNome_com_ref(nomes, nome);
+        if(string.IsNullOrWhiteSpace(nomeAchado)) WriteLine("Nome não se encontra na lista");
+        else{
+            WriteLine("Para qual nome você quer mudar? ");
+            var nomeNovo = ReadLine();
+            nomeAchado = nomeNovo;
+            WriteLine($"Lista de nomes: {string.Join(", \n", nomes)}");
+        }
     }
 }
